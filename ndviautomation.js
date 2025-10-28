ff364a27136423963f0a381d4014bdf010042787
madjidsadou <154760509+madjidsadou@users.noreply.github.com>
2025-10-28 20:15:23 +0100

    try

--- ndviautomation.js
+++ ndviautomation.js
@@ -1,25 +1,46 @@
-var map = new L.Map('map', {
-    center:[46.53584966676345,6.626950549641442],
-    // maxBounds: [[44.8, 8.2], [47.5, 9]],
-    minZoom: 2,
-    maxZoom: 20,
-    zoom: 3
+var map = L.map('map', {
+  center: [46.5358, 6.6269],
+  zoom: 8,
+  maxBounds: [[44.8, 8.2], [47.5, 9]],
+  minZoom: 2,
+  maxZoom: 20
 });
 
-// Define tile layers
-var osmLayer = L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
-});
-var esriImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-  attribution: '&copy; <a href="http://www.esri.com">Esri</a>, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-});
+// Add base layer
+L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
 
-osmLayer.addTo(map);
+// Add Leaflet SVG overlay
+L.svg().addTo(map);
+const svg = d3.select(map.getPanes().overlayPane).select("svg");
+const g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
-// Define base layers
-var baseLayers = {
-  "OpenStreetMap": osmLayer,
-  "Photos aériennes ESRI": esriImagery,
-};
+d3.json("geowgs.geojson").then(function(data) {
+
+  // D3 path generator using Leaflet projection
+  const path = d3.geoPath().projection(d3.geoTransform({
+    point: function(x, y) {
+      const point = map.latLngToLayerPoint([y, x]); // Leaflet expects [lat, lon]
+      this.stream.point(point.x, point.y);
+    }
+  }));
+
+  const feature = g.selectAll("path")
+    .data(data.features)
+    .enter()
+    .append("path")
+    .attr("fill", "none")
+    .attr("stroke", "#000000ff")
+    .attr("stroke-width", 1);
+
+  function reset() {
+    feature.attr("d", path);
+  }
+
+  // Redraw on map zoom/pan
+  map.on("zoom viewreset move", reset);
+  reset();
+});
 
 
